b755a50135e1969e775470dc7e5ee46e5bd6d504
haru <haru@inl.ics.keio.ac.jp>
2017-12-01 21:41:40 +0900

    Remove unnecessary include headers from udpecho

--- udpecho/udpecho.c
+++ udpecho/udpecho.c
@@ -1,18 +1,11 @@
-#include <sys/param.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <ctype.h>
-#include <errno.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sysexits.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #define PORT_NUM 8888
 
--- udpecho/udpechod.c
+++ udpecho/udpechod.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-#include <sys/param.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <ctype.h>
-#include <errno.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sysexits.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #define IP_ADDR "127.0.0.1"
 #define PORT_NUM 8888
