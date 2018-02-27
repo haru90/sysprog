5db96941c710d34ffcb90778a3890019afe1a0b1
haru <haru@inl.ics.keio.ac.jp>
2018-02-27 09:29:26 +0900

    Add dijkstra.c

--- dijkstra/dijkstra.c
+++ dijkstra/dijkstra.c
@@ -5,9 +5,40 @@
 #define NNODE 6
 #define INF 100 // infinity
 
-void dijkstra(int cost[NNODE][NNODE], int i, int dist[NNODE], int prev[NNODE])
+void dijkstra(int cost[NNODE][NNODE], int u, int dist[NNODE], int prev[NNODE])
 {
-    
+    int determined_n[NNODE] = {0};
+    int determined_n_num = 0;
+    int v, w;
+    int i;
+    int min;
+
+    determined_n[u] = 1;
+    determined_n_num++;
+    for (v = 0; v < NNODE; v++) {
+        if (cost[u][v] != INF) {
+            dist[v] = cost[u][v];
+            prev[v] = u;
+        } else
+            dist[v] = INF;
+    }
+
+    while (determined_n_num != NNODE) {
+        for (i = 0, min = INF; i < NNODE; i++) {
+            if (!determined_n[i] && dist[i] < min) {
+                min = dist[i];
+                w = i;
+            }
+        }
+        determined_n[w] = 1;
+        determined_n_num++;
+        for (v = 0; v < NNODE; v++) {
+            if (cost[w][v] != INF && !determined_n[v] && dist[v] > dist[w] + cost[w][v]) {
+                dist[v] = dist[w] + cost[w][v];
+                prev[v] = w;
+            }
+        }
+    }
 }
 
 int main(void)
@@ -22,9 +53,9 @@ int main(void)
     };
     char node_names[NNODE] = {'A', 'B', 'C', 'D', 'E', 'F'};
 
+    int i, j;
     int dist[NNODE];
     int prev[NNODE];
-    int i, j;
 
     for (i = 0; i < NNODE; i++) {
         dijkstra(cost, i, dist, prev);
